60af62f24a4a48ca3d3d7fa11a514444b450e841
ShahHet2812 <het48155@gmail.com>
2025-03-18 13:27:09 +0530

    Add files via upload

---
+++ B4_128_Sem_4/T1/FSD/j1.js
@@ -0,0 +1,17 @@
+// var a={
+//     "name":"abc",
+//     "age":20,
+//     "subjects":{"FSD-2":"NAS","FCSP-2":"TAT"},//Object dtype
+//     "marks":[24,22,18,17],//array dtype
+//     "ispass":true, //boolean dtype
+//     "test":null    //null dtype
+// }
+// console.log(a.name)
+// console.log(a['name'])
+// console.log(a.age)
+// console.log(a.subjects["FSD-2"])
+// console.log(a.marks[2])
+// console.log(a.ispass)
+// console.log(a.test)
+var data = require("./j1.json");
+console.log(data);
