f7d40dd394eecf914579d7309579a4c5e1fc37c1
ShahHet2812 <het48155@gmail.com>
2025-07-11 13:26:12 +0530

    Add files via upload

---
+++ B4_128_Sem_4/movieratings/accounts/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from accounts import views
+urlpatterns = [
+    path('signupaccount/',views.signupaccount,name='signupaccount'),
+    path('logout/',views.logoutaccount,name='logoutaccount'),
+    path('loginaccount/',views.loginaccount,name='loginaccount'),
+]
--- B4_128_Sem_4/movieratings/movieratings/urls.py
+++ B4_128_Sem_4/movieratings/movieratings/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('movie.urls')),
     path('news/',include('news.urls')),
+    path('accounts/',include('accounts.urls')),
     path('movie/',include('movie.urls')),
 ]
 urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
